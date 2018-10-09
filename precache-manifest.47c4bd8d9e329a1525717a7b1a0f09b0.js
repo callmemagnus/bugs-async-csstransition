c803435fef72242ad09934a5c7cd2053b688b6a9
Magnus Anderssen <magnus.anderssen@nespresso.com>
2018-10-09 09:43:12 +0200

    Updates

---
+++ precache-manifest.47c4bd8d9e329a1525717a7b1a0f09b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fad155afcd6c49946ad4",
+    "url": "/bugs-async-csstransition/static/js/runtime~main.fad155af.js"
+  },
+  {
+    "revision": "17f89363b24211f0c488",
+    "url": "/bugs-async-csstransition/static/js/main.17f89363.chunk.js"
+  },
+  {
+    "revision": "9b1076673450a88a7658",
+    "url": "/bugs-async-csstransition/static/js/2.9b107667.chunk.js"
+  },
+  {
+    "revision": "8697d7d5b45a32dedbff",
+    "url": "/bugs-async-csstransition/static/js/1.8697d7d5.chunk.js"
+  },
+  {
+    "revision": "17f89363b24211f0c488",
+    "url": "/bugs-async-csstransition/static/css/main.9d43a7d4.chunk.css"
+  },
+  {
+    "revision": "d83b17dd71fc180a24b856bb6d8e083a",
+    "url": "/bugs-async-csstransition/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/bugs-async-csstransition/precache-manifest.47c4bd8d9e329a1525717a7b1a0f09b0.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/bugs-async-csstransition/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
