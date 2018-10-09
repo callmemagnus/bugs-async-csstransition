5c1aefaeeb8df6702434ac8f401dabbbd288e889
Magnus Anderssen <magnus.anderssen@nespresso.com>
2018-10-09 10:01:00 +0200

    simplify

--- src/App.js
+++ src/App.js
@@ -1,43 +1,62 @@
-import React, { Component } from 'react';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import React, { Component } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
-import './App.css';
-import asyncComponent from './asyncComponent';
-import ImportedComponent from './ImportedComponent';
+import "./App.css";
+import asyncComponent from "./asyncComponent";
+import ImportedComponent from "./ImportedComponent";
 
-const AsyncComponent = asyncComponent(() => import('./AsyncLoadedComponent'))
+const AsyncComponent = asyncComponent(() => import("./AsyncLoadedComponent"));
 
 class App extends Component {
-  showAsync: false
-  state = {
-  }
+  showAsync: false;
+  state = {};
   toggleClick() {
-    this.setState({ showAsync: !this.state.showAsync })
+    this.setState({ showAsync: !this.state.showAsync });
   }
   renderAsyncComponentWithAnimation() {
     return (
-      <CSSTransition in={this.state.showAsync} classNames="transition-async" timeout={10000}>
+      <CSSTransition
+        in={this.state.showAsync}
+        classNames="transition"
+        timeout={10000}
+      >
         <AsyncComponent />
       </CSSTransition>
-    )
+    );
   }
   renderComponentWithAnimation() {
     return (
-      <CSSTransition in={this.state.showAsync} classNames="transition-import" timeout={10000}>
+      <CSSTransition
+        in={this.state.showAsync}
+        classNames="transition"
+        timeout={10000}
+      >
         <ImportedComponent />
       </CSSTransition>
-    )
+    );
   }
   render() {
     return (
       <div className="App">
         <header>header</header>
-        <main style={{ display: "flex", flexDirection: 'column', alignItems: 'center' }}>
+        <main
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center"
+          }}
+        >
           <p>
             <button onClick={this.toggleClick.bind(this)}>Click me</button>
           </p>
           <TransitionGroup>
-            <div style={{ display: "grid", gridGap: "20px", gridTemplateColumns: "1fr 1fr" }}>
+            <div
+              style={{
+                display: "grid",
+                gridGap: "20px",
+                gridTemplateColumns: "1fr 1fr"
+              }}
+            >
               {this.state.showAsync && this.renderComponentWithAnimation()}
               {this.state.showAsync && this.renderAsyncComponentWithAnimation()}
             </div>
